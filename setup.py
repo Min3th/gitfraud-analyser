593d94d1d60c89898db471678c710480fec04b0c
Min3th <minethweerasinghe@gmail.com>
2025-07-04 00:08:16 +0530

    fix issues in setup.py

--- setup.py
+++ setup.py
@@ -3,28 +3,29 @@ from setuptools import setup,find_packages
 with open("README.md","r",encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt","r",encoding="utf-8") as fh:
-    requirements = fh.read()
+    requirements = fh.read().splitlines()
 
 setup(
-    name='mytool',
+    name='gitfraud',
     version= '0.01',
     author= 'John Doe',
     author_email = "mmm@gmail.com",
-    license='', # text indicating the license covering the package
-    description = 'just a tool', 
+    license='MIT', # text indicating the license covering the package
+    description = 'A CLI tool to deteck fake Github commits', 
     long_description = long_description,
     long_description_content_type="text/markdown",
     url='github url of tool code',
-    py_modules=['app'],
-    packages=find_packages(),
-    install_requires = [requirements],
+    py_modules=['main'],
+    # packages=find_packages(),
+    install_requires = [], # add 'requirements' in the future
     python_requires = '>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3.13.0",
         "Operating System :: OS Independent",
     ],
-    entry_points = '''
-        [console_scripts]
-        cooltool=my_tool:cli
-'''
+    entry_points = {
+        'console_scripts':[
+            'gitfraud=main:analyze' # checks for analyze function in main.py
+        ]
+    }
 )
