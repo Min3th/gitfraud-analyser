51555527b872672bd3e92c4f7986ebeec871c84d
Min3th <minethweerasinghe@gmail.com>
2025-07-04 22:33:21 +0530

    add function to collect and save data from commits.

---
+++ ML/collect_data.py
@@ -0,0 +1,40 @@
+import os
+import csv
+from dotenv import load_dotenv 
+from features import extract_features
+from main import fetch_global_commits
+
+load_dotenv()
+
+def collect_and_save(username,label,output_file="data/commits.csv"):
+    token = os.getenv("GITHUB_TOKEN")
+    commits = fetch_global_commits(username,token)
+
+    if not commits:
+        print("No commits found for the user!")
+        return
+    
+    field_names = ["username","repo","message","label","msg_length","is_generic_msg","hour_of_day","lines_added",
+                   "sus_lines","files_changed"]
+    
+    os.makedirs("data",exist_ok=True)
+
+    with open(output_file,mode="a",newline="",encoding="utf-8") as file:
+        writer = csv.DictWriter(file,fieldnames=field_names)
+
+        if file.tell() == 0:
+            writer.writeheader()
+
+        for commit in commits:
+            features = extract_features(commit)
+            row = {
+                "username":username,
+                "repo":commit["repo"],
+                "message":commit["message"],
+                "label":label
+            }
+
+            row.update(features)
+            writer.writerow(row)
+
+    print(f"✅ {len(commits)} commits saved for {username}.")
